32c4cac8adfb5bf8b0352466db07e7b871174e2f
tushar1999sharma <tushar1999sharma@gmail.com>
2020-06-11 00:43:17 +0530

    Updates

---
+++ precache-manifest.bb98a0dc768df366d15284bdf43724f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b5a3b861df76ecab30a49287828d2ea",
+    "url": "/React-ToDo-List/index.html"
+  },
+  {
+    "revision": "0d70577c465518f56364",
+    "url": "/React-ToDo-List/static/css/main.903453b4.chunk.css"
+  },
+  {
+    "revision": "9a94caee4f520b3bf660",
+    "url": "/React-ToDo-List/static/js/2.e24af144.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-ToDo-List/static/js/2.e24af144.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d70577c465518f56364",
+    "url": "/React-ToDo-List/static/js/main.2eafa111.chunk.js"
+  },
+  {
+    "revision": "35e2be4d1f4cb1ec4a8e",
+    "url": "/React-ToDo-List/static/js/runtime-main.8171c8bc.js"
+  }
+]);
